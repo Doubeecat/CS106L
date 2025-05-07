1bc86d1176dd43d0f276dca95e0deb593911645c
Doubeecat <1359446223@qq.com>
2025-05-07 23:52:03 +0800

    notes modified

--- test.cpp
+++ test.cpp
@@ -5,74 +5,17 @@ Undo the destiny.
 #include "fraction.h"
 using namespace std;
 #define ll long long
-fraction fraction::operator + (const fraction &a) {
-    int newbase = base * a.base;
-    int newfrac = a.base * frac + base * a.frac;
-    int g = __gcd(newbase,newfrac);
-    return fraction(newbase/g,newfrac/g);
-}
-fraction fraction::operator + (const int &a) {
-    int newbase = base;
-    int newfrac = frac + a * base;
-    int g = __gcd(newbase,newfrac);
-    return fraction(newbase/g,newfrac/g);
-}
-fraction fraction::operator - (const fraction &a) {
-    int newbase = base * a.base;
-    int newfrac = a.base * frac - base * a.frac;
-    int g = __gcd(newbase,newfrac);
-    return fraction(newbase/g,newfrac/g);
-}
-fraction fraction::operator * (const fraction &a) {
-    int newbase = base * a.base;
-    int newfrac = frac * a.frac;
-    int g = __gcd(newbase,newfrac);
-    return fraction(newbase/g,newfrac/g);
-}
-fraction fraction::operator / (const fraction &a) {
-    int newbase = base * a.frac;
-    int newfrac = frac * a.base;
-    int g = __gcd(newbase,newfrac);
-    return fraction(newbase/g,newfrac/g);
-}
-fraction& fraction::operator += (const fraction &a) {
-    
-    frac = a.base * frac + base * a.frac;
-    base *= a.base;
-    int g = __gcd(base,frac);
-    base /= g,frac /= g;
-    return *this;
-}
-fraction& fraction::operator -= (const fraction &a) {
-    
-    frac = a.base * frac - base * a.frac;
-    base *= a.base;
-    int g = __gcd(base,frac);
-    return *this;
-}
-fraction& fraction::operator = (const fraction &a){
-    base = a.base;
-    frac = a.frac;
-    return *this;
+
+template <typename T>
+void write(T x) {
+    cout << "This is template" << x << "\n";
 }
 
-ostream& operator << (ostream& os,const fraction &a) {
-    os << "(" << a.frac << "/" << a.base << ")";
-    return os;
-}
-template <class T,class Container = std::vector <T> > 
-class Stack {
-    public:
-        Stack() = default;
-        ~Stack() = default;
-    private:
-        vector <T> s;
+void write(int x) {
+    cout << "This is specific" << x << "\n";
 }
 
 int main() {
-    fraction a(2,1),b(4,3),c(6,5);
-    cout << a << " " << b <<  " " << c << "\n";
-    cout << a + b;
-    cout << b + c;
-    cout << ((a += b) += c);
+    int a = 1;
+    write(a);    
 }
